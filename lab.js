c958f7bf2b92b1218d6737ae69027399540ebd72
Devin Lorenz <devinlorenz.work@gmail.com>
2022-06-22 17:00:36 -0400

    Figured out while loop for problem 11

--- lab.js
+++ lab.js
@@ -172,31 +172,38 @@ let z = 5;
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
-while (z > 0) {
-    console.log(z)
-  z -= 1
-}
-z = 4
+// while (z > 0) {
+//     console.log(z)
+//   z -= 1
+// }
+// z = 4
+
+// while (z > 0) {
+//     console.log(z)
+//   z -= 1
+// }
+// z = 3
+
+// while (z > 0) {
+//     console.log(z)
+//   z -= 1
+// }
+// z = 2
+
+// while (z > 0) {
+//     console.log(z)
+//   z -= 1
+// }
+// z = 1
+
+// while (z > 0) {
+//     console.log(z)
+//   z -= 1
+// }
 
 while (z > 0) {
-    console.log(z)
-  z -= 1
-}
-z = 3
-
-while (z > 0) {
-    console.log(z)
-  z -= 1
-}
-z = 2
-
-while (z > 0) {
-    console.log(z)
-  z -= 1
-}
-z = 1
-
-while (z > 0) {
-    console.log(z)
+  for (let p = z ; p >= 1 ; p--){
+     console.log(p)
+  }
   z -= 1
 }
